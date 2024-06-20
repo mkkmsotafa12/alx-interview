88598d4a7ce9af72e309b0f962288ea4f7a040a9
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-06-20 10:27:55 -0400

    0. Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,39 @@
 #!/usr/bin/python3
 """ A script that reads stdin line by line and computes metrics """
 
-import sys
 
+from sys import stdin
+import re
 
-i = 0
-sum_file_size = 0
-status_codes = {'200': 0,
-                '301': 0,
-                '400': 0,
-                '401': 0,
-                '403': 0,
-                '404': 0,
-                '405': 0,
-                '500': 0}
-
+pattern = r'^[\d+\.]+ - \[[\d+-: ]+\] \S+ \/\S+ \S+ (\d+) (\d+)$'
+total_size = 0
+count = 0
+status_dic = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0,
+              405: 0, 500: 0}
 try:
-    for line in sys.stdin:
-        args: list[str] = line.split(' ')
-        if len(args) > 2:
-            status_line: str = args[-2]
-            file_size: str = args[-1]
-            if status_line in status_codes:
-                status_codes[status_line] += 1
-            sum_file_size += int(file_size)
-            i += 1
-            if i == 10:
-                print('File size: {:d}'.format(sum_file_size))
-                sorted_keys: list[str] = sorted(status_codes.keys())
-                for key in sorted_keys:
-                    value = status_codes[key]
-                    if value != 0:
-                        print('{}: {}'.format(key, value))
-                i = 0
-except Exception:
-    pass
-finally:
-    print('File size: {:d}'.format(sum_file_size))
-    sorted_keys = sorted(status_codes.keys())
-    for key in sorted_keys:
-        value: int = status_codes[key]
-        if value != 0:
-            print('{}: {}'.format(key, value))
+    for line in stdin:
+        match = re.match(pattern, line)
+        if match:
+            status, size = match.groups()
+            try:
+                total_size += int(size)
+                status_dic[int(status)] += 1
+            except Exception:
+                pass
+            count += 1
+        else:
+            print('false')
+            continue
+
+        if count == 1 or count == 10:
+            print('File size:', total_size)
+            for key, value in status_dic.items():
+                if value > 0:
+                    print(f'{key}: {value}')
+            count = 0
+except KeyboardInterrupt:
+    print('File size:', total_size)
+    for key, value in status_dic.items():
+        if value > 0:
+            print(f'{key}: {value}')
+        count = 0
